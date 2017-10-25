82e0e1acaf857126d1a55db33a9bd27311da912d
Caroline Olijve <olijvecaroline@gmail.com>
2017-10-25 14:56:21 +0200

    edit tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,7 +15,7 @@ def setup
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
