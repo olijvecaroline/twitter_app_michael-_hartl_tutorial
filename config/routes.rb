91ba84560e249a64d9aa0459b7f5413c1bb90372
Caroline Olijve <olijvecaroline@gmail.com>
2017-10-27 09:51:53 +0200

    generate User model

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get  '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/sign_up', to: 'users#new', as: 'sign_up'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
