7f04927d72b6981b21d0238c8be4fa038b8990d8
Caroline Olijve <olijvecaroline@gmail.com>
2017-10-27 14:11:53 +0200

    add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
 					  length: { maximum: 244 }, 
 					  format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: {case_sensitive: false}
+	validates :password, presence: true,
+						 length: { minimum: 6 }
+	
+
+	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	def setup
-		@user= User.new(name: 'example', email: 'example@example.com')
+		@user= User.new(name: 'example', email: 'example@example.com', password: "foobar", password_confirmation: "foobar")
 	end
   # test "the truth" do
   #   assert true
@@ -57,12 +57,23 @@ class UserTest < ActiveSupport::TestCase
 	    assert_not duplicate_user.valid?
   	end	
 
-  	test "email addresses should be downcased" do
-	    mixed_case_email = "EXAMPLE@user.com"
+  	test "email addresses should be saved as lower-case" do
+	    mixed_case_email = "Foo@ExAMPle.CoM"
 	    @user.email = mixed_case_email
 	    @user.save
-	    assert_equal @user.reload.email, mixed_case_email.downcase
-	    
-  	end	
+	    reloaded = @user.reload.email
+	    assert_equal mixed_case_email.downcase, reloaded
+  	end
+
+  	test "password should be longer then 5 characters" do
+  	  @user.password = @user.password_confirmation = "a" * 5
+  	  assert_not @user.valid?
+  	end
+
+  	test "password should be present" do
+  	  @user.password = @user.password_confirmation = "  "
+  	  assert_not @user.valid?
+  	end
+
 
 end
