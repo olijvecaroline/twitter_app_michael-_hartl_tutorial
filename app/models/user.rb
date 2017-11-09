18acea65238d7478b45738a3fce74cf96d7e4a98
Caroline Olijve <olijvecaroline@gmail.com>
2017-11-09 10:49:53 +0100

    commit before deploying

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
-	before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
+
+
 	validates :name, presence: true, length: { maximum: 50 }
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -38,6 +41,17 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
   	return false if remember_digest.nil?
   	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email
+     self.email.downcase!
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :account_activations, only: [:edit]
+
  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
